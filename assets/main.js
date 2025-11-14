86a0242c866e7eea5c0a5bb19b6842d8d984d0d0
pasdepi <eva.bassam.deeb@gmail.com>
2025-11-14 22:47:55 +0100

    Implement draggable main element with close button functionality and update HTML structure

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,40 @@
+dragElement(document.getElementById("main"));
 
+function dragElement(elmnt) {
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    const bar = document.getElementById("bar");
+    const dragTarget = bar || elmnt;
+
+    dragTarget.addEventListener("mousedown", dragMouseDown);
+
+    const closeButton = document.querySelector(".X");
+    if (closeButton) {
+        closeButton.addEventListener("click", e => {
+            e.stopPropagation(); 
+            elmnt.style.display = "none";
+        });
+    }
+
+    function dragMouseDown(e) {
+        e.preventDefault();
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.addEventListener("mousemove", elementDrag);
+        document.addEventListener("mouseup", closeDragElement);
+    }
+
+    function elementDrag(e) {
+        e.preventDefault();
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        document.removeEventListener("mousemove", elementDrag);
+        document.removeEventListener("mouseup", closeDragElement);
+    }
+}
